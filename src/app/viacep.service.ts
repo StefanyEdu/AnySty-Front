600f1371464d1caeb6b29b9b8181e29d39f9dffb
StefanyEdu <ostefany20@gmail.com>
2021-11-05 14:49:54 -0300

    1 file modified

--- src/app/viacep.service.ts
+++ src/app/viacep.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
+import { Cep } from './ceps';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn:'root'
+  providedIn: 'root',
 })
 export class ViacepService {
-
-  constructor() { }
-
+  constructor() {}
 }
